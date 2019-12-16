534fa89dbbd2235018cc6f1f8ab9270ae8f9a7f7
Mayu Imanaka <imamayu.1215@gmail.com>
2019-12-16 08:59:46 -0800

    update:index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,6 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 
-<<<<<<< HEAD
-// import Bio from "../components/bio"
-import Layout from "../components/Layout/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
-// import Button from "../components/button"
-
-class Blog extends React.Component {
-  render() {
-    const siteTitle = "Signalog"
-    const { data } = this.props
-    const posts = data.allMdx.edges
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <div style={{ margin: "20px 0 40px" }}>
-          {posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <div key={node.fields.slug}>
-                <h3
-                  style={{
-                    marginBottom: rhythm(1 / 4),
-                  }}
-                >
-                  <Link
-                    style={{ boxShadow: `none` }}
-                    to={`blog${node.fields.slug}`}
-                  >
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </div>
-            )
-          })}
-        </div>
-=======
 import Layout from "../components/Layout/layout";
 import Img from "gatsby-image"
 
@@ -78,39 +34,12 @@ class Blog extends React.Component {
             </div>
           </div>
         </main>
->>>>>>> develop
       </Layout>
     )
   }
 }
 
 export default Blog
-<<<<<<< HEAD
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-=======
 export const pageQuery = graphql`
   query {
           site {
@@ -141,5 +70,4 @@ export const pageQuery = graphql`
 }
 }
 }
->>>>>>> develop
 `
