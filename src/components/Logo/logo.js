66b77ec81d4f0a7faed0b614e744c3158d8a6a05
Mayu Imanaka <imamayu.1215@gmail.com>
2019-12-19 15:23:07 -0800

    update:logo and burger menu

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -26,8 +26,7 @@ const Logo = () => (
         <circle cx="24.178" cy="24.178" r="21.678" fill="none" />
       </g>
       <path id="signal-roof3" data-name="signal-roof3" d="M196.226,286.033a32.487,32.487,0,0,1,60.387-1.373" transform="translate(-43.003 277.795)" fill="none" stroke="#2f2f2f" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="6" />
-
-      <text id="signal-name" transform="translate(113 661.355)" fontSize="45" fontFamily="Atrament-Italic, Atrament" fontStyle="italic"><tspan x="0" y="0">SIGNALOG</tspan></text>
+      <text id="signal-name" transform="translate(113 661.355)"><tspan x="0" y="0">SIGNALOG</tspan></text>
     </g>
   </svg>
 )
