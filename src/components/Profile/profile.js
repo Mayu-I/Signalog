d4df5d74f40171dbda44ccb96f6869f6506ba9f9
Mayu Imanaka <imamayu.1215@gmail.com>
2020-02-19 14:30:26 -0800

    add:20-01-14 images

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -6,7 +6,7 @@ const Profile = () => (
     <div className="profile">
         <div className="profileImg"><img src={profileImg} alt="" /></div>
         <h2 className="about__name">MAYU</h2>
-        <p className="profileDesc">バンクーバー在住、20歳。<br />高三の時に朝井リョウの「何者」を読んで社会人になるのが怖くなり、大学受験から逃亡。<br />高校を卒業し、何にやりたいのか特に決まらないまま、英語を話せるようになりたいという重いだけで語学学校に半年通う。<br />その間に現地で仕事に就きたいと思うようになり、コーディングを始める。<br />一時帰国してからは、東京のWeb制作会社で1年間フロントエンドのバイトをしてました。<br />今はまたバンクーバーに戻り、<a href="https://redacademy.com/vancouver/" target="_blank" rel="noopener noreferrer">RED Academy</a>で現地就職を目指して英語にもコーディングにも苦戦しながらも勉強中。</p>
+        <p className="profileDesc">バンクーバー在住、20歳。<br />高三の時に朝井リョウの「何者」を読んで社会人になるのが怖くなり、大学受験から逃亡。<br />高校を卒業し、何にやりたいのか特に決まらないまま、英語を話せるようになりたいという重いだけで語学学校に半年通う。<br />その間に現地で仕事に就きたいと思うようになり、コーディングを始める。<br />一時帰国してからは、東京のWeb制作会社で1年間フロントエンドのバイトをしてました。<br />今はまたバンクーバーに戻り、<a href="https://redacademy.com/vancouver/" target="_blank" rel="noopener noreferrer">RED Academy</a>で現地就職を目指して勉強中。</p>
     </div>
 );
 
